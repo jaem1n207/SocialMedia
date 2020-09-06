33d3ee2e73ed95801d6598d1dcadb810750a7a96
jaem1n207 <jaem1n207@naver.com>
2020-09-06 16:05:28 +0900

    Add: users table

---
+++ back/models/user.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize/types');
+
+module.exports = (sequelize, DataTypes) => {
+  // create users table
+  const User = sequelize.define(
+    'User',
+    {
+      email: {},
+      nickname: {},
+      password: {},
+    },
+    {
+      // 한글 셋팅
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    }
+  );
+  User.associate = (db) => {};
+  return User;
+};
