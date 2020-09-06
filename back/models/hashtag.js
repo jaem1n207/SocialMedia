be1e9cc64402cbf3cc5a35f8aa3121dc101b51d3
jaem1n207 <jaem1n207@naver.com>
2020-09-06 16:12:17 +0900

    Add: hashtags table

---
+++ back/models/hashtag.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const Hashtag = sequelize.define(
+    'Hashtag',
+    {
+      name: {},
+    },
+    {
+      // 한글 + 이모티콘 셋팅
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    }
+  );
+  Hashtag.associate = (db) => {};
+
+  return Hashtag;
+};
