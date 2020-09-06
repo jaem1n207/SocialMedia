58eebf6a303a72dac1c89a9d4e2dc28f28571807
jaem1n207 <jaem1n207@naver.com>
2020-09-06 16:10:44 +0900

    Add: comments table

---
+++ back/models/comment.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  // create comments table
+  const Comment = sequelize.define(
+    'Comment',
+    {
+      content: {},
+    },
+    {
+      // 한글 + 이모티콘 셋팅
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    }
+  );
+  Comment.associate = (db) => {};
+
+  return Comment;
+};
--- back/models/user.js
+++ back/models/user.js
@@ -14,5 +14,6 @@ module.exports = (sequelize, DataTypes) => {
     }
   );
   User.associate = (db) => {};
+
   return User;
 };
