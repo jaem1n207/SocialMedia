37bec1d19c82ca451fad00609b35beaf6b3082d4
jaem1n207 <jaem1n207@naver.com>
2020-09-14 17:16:37 +0900

    Update: hash password & exUser email

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const { User } = require('../models');
 
 const router = express.Router();
 
 /* Signup */
-router.post('/', async (req, res) => {
-  await User.create({
-    email: req.body.email,
-    nickname: req.body.nickname,
-    password: req.body.password,
-  });
-  res.send('ok');
+router.post('/', async (req, res, next) => {
+  try {
+    const exUser = await User.findOne({
+      // 똑같은 email을 가진 user가 없다면 null값 할당
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (exUser) {
+      return res.status(403).send('이미 사용중인 아이디입니다.');
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    await User.create({
+      email: req.body.email,
+      nickname: req.body.nickname,
+      password: hashedPassword,
+    });
+    res.send('ok');
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 module.exports = router;
