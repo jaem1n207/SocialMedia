175786b3fc06fb54e2d7a258bb8d52b01e2c4ca2
jaem1n207 <jaem1n207@naver.com>
2020-09-16 15:40:24 +0900

    Add: kakaoStrategy

--- back/passport/kakaoStrategy.js
+++ back/passport/kakaoStrategy.js
@@ -1,2 +1,38 @@
+const passport = require('passport');
 const KakaoStrategy = require('passport-kakao').Strategy;
 const { User } = require('../models');
+
+module.exports = (passport) => {
+  passport.use(
+    new KakaoStrategy(
+      {
+        clientID: process.env.KAKAO_ID,
+        callbackURL: '/auth/kakao/callback',
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          const exUser = await User.fine({
+            where: {
+              snsId: profile.id,
+              provider: 'kakao',
+            },
+          });
+          if (exUser) {
+            done(null, exUser);
+          } else {
+            const newUser = await User.create({
+              email: profile._json && profile._json.kaccount_email,
+              nickname: profile.displayName,
+              snsId: profile.id,
+              provider: 'kakao',
+            });
+            done(null, newUser);
+          }
+        } catch (err) {
+          console.error(err);
+          done(err);
+        }
+      }
+    )
+  );
+};
