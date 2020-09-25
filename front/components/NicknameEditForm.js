84cec12050b0ceb405fe68dfde884aa70ca7827a
jaem1n207 <jaem1n207@naver.com>
2020-09-25 21:05:53 +0900

    Update: NicknameEditForm

--- front/components/NicknameEditForm.js
+++ front/components/NicknameEditForm.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from 'antd';
 import { css } from '@emotion/core';
 
-const NicknameEditForm = () => (
-  <Form
-    css={css`
-      margin-bottom: 20px;
-      border: 1px solid #d9d9d9;
-      padding: 20px;
-    `}
-  >
-    <Input.Search addonBefore="닉네임" enterButton="수정" />
-  </Form>
-);
+import useInput from '../hooks/useInput';
+import { CHANGE_NICKNAME_REQUEST } from '../reducers/user';
+
+const NicknameEditForm = () => {
+  const { me } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const [nickname, onChangeNickname] = useInput(me?.nickname || '');
+
+  /* 닉네임 수정 */
+  const onSubmit = useCallback(() => {
+    dispatch({
+      type: CHANGE_NICKNAME_REQUEST,
+      data: nickname,
+    });
+  }, [nickname]);
+
+  return (
+    <Form
+      css={css`
+        margin-bottom: 20px;
+        border: 1px solid #d9d9d9;
+        padding: 20px;
+      `}
+    >
+      <Input.Search
+        value={nickname}
+        onChange={onChangeNickname}
+        onSearch={onSubmit}
+        addonBefore="닉네임"
+        enterButton="수정"
+      />
+    </Form>
+  );
+};
 
 export default NicknameEditForm;
